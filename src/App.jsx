22ec3d4dc5458014fe3116bbcfa00e72bf371511
speedchatanon <87306469+speedchatanon@users.noreply.github.com>
2021-09-29 07:26:30 +0700

    add file

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [data, setData] = useState('')
+
+  useEffect( async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    setData(JSON.stringify(data, null, "\t"))
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+      <pre>
+        {data}
+      </pre>
+    </div>
+  )
+}
+
+export default App
